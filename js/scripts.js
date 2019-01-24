f68c61b7d1a40dc9cfb7dbcef0a159025ea3124b
Joe Pritchett and Tate Easterbrooks <tate@>
2019-01-24 14:40:34 -0800

    add ability to translate single words to pig latin

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 function translate(phrase)  {
-  var firstHalf = []
-  var secondHalf = []
-  var vowels = ["a","e","i","o","u"];
+  var firstHalf = "";
+  var secondHalf = "";
+  var vowels = ["a","e","i","o","u","A","E","I","O","U"];
   if (vowels.includes(phrase[0]))  {
-    alert(phrase + "way")
+    return (phrase + "way");
   } else {
-    for (var i = 0; i < phrase.length; i++) {
+      for (var i = 0; i < phrase.length;) {
+        if (!vowels.includes(phrase[i])) {
+          i++
+        } else {
+          firstHalf = phrase.slice(0, i);
+          secondHalf = phrase.slice(i, phrase.length);
+          return (secondHalf + firstHalf + "ay");
+
+        }
     }
   }
 }
@@ -15,5 +23,7 @@ $(document).ready(function(event) {
     event.preventDefault();
     var phraseInput = $("input#phrase").val();
     translate(phraseInput);
+
+    $("#result").text(translate(phraseInput));
   });
 });
