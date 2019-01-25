2b29135f476edb99e63dc33c2112afe63b3c517d
Joe Pritchett and Tate Easterbrooks <tate@>
2019-01-24 16:13:01 -0800

    add ability to differentiate if Y is the first character or not, then translate accordingly

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 function translate(phrase)  {
   var firstHalf = "";
   var secondHalf = "";
-  var vowels = ["a","e","i","o","u","A","E","I","O","U"];
+  var vowels = ["a","e","i","o","u","y","A","E","I","O","U","Y"];
+  var vowelsNoY = ["a","e","i","o","u","A","E","I","O","U"];
+  var q = ["Q","q"]
+  var u = ["U","u"]
+  if (phrase[0] == "y" || phrase[0] == "Y") {
+    for (var i = 0; i < phrase.length;) {
+      if (!vowelsNoY.includes(phrase[i])) {
+        i++
+      } else {
+        firstHalf = phrase.slice(0, i);
+        secondHalf = phrase.slice(i, phrase.length);
+        return (secondHalf + firstHalf + "ay");
+      }
+    }
+  }
   if (vowels.includes(phrase[0]))  {
     return (phrase + "way");
   } else {
